ce324529e818debe294de7412674cd0225ae42b5
RFG\hirokazuiida0515 <hirokazu.hi.iida@jp.ricoh.com>
2018-07-13 17:05:21 +0900

    組み込んだけどうまく動作しない

--- Django_sample/myenv/blog/views.py
+++ Django_sample/myenv/blog/views.py
@@ -1,9 +1,35 @@
+# coding: utf-8
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import numpy as np
+import sys
+
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
     
+    # 学習済みのVGG16をロード
+    # 構造とともに学習済みの重みも読み込まれる
+    model = VGG16(weights='imagenet')
+
+    # 引数で指定した画像ファイルを読み込む
+    # サイズはVGG16のデフォルトである224x224にリサイズされる
+    img = image.load_img("banana.jpg", target_size=(224, 224))
+
+    # 読み込んだPIL形式の画像をarrayに変換
+    x = image.img_to_array(img)
+
+    # 3次元テンソル（rows, cols, channels) を
+    # 4次元テンソル (samples, rows, cols, channels) に変換
+    # 入力画像は1枚なのでsamples=1でよい
+    x = np.expand_dims(x, axis=0)
+
+    # Top-5のクラスを予測する
+    # VGG16の1000クラスはdecode_predictions()で文字列に変換される
+    preds = model.predict(preprocess_input(x))
+    results = decode_predictions(preds, top=5)[0]
+
+    return render(request, 'blog/post_list.html', {})
